4917e291ab82d7b95b490620ec9a7fb86938db58
homuhomuhyper <is0453hh@ed.ritsumei.ac.jp>
2019-02-19 23:37:44 +0900

    changed accesstoken and channelsecret

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ from linebot.models import (
 )
 import os
 
-app = Flask(__name__)
+app = Flask(__name__)i
 
 #環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["ICkb9WxPciMbNUs3aX3CM3SOuI81me2EL2aWZbMmHRMzOv1hCtmmHDodTdoY9ioUtR3ZAce/1MAkGSI0NGS9N2+s89DdMsLyk7RKpE5Ur49yir5FMvWlOdZhT09gPMdqKUMxI2QHfcl1OyvA8PsDMwdB04t89/1O/w1cDnyilFU="]
-YOUR_CHANNEL_SECRET = os.environ["5784d802f2b0ad193ee9fdd354197289"]
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["wLNBgYDgXqPR0Lw+smPNBJ9ToSx4NQnJgbJuCt7UYvl3tItaT2zELErsn/vXExcuV6YNbi6Ni0hxjzlwQtTk5XlT3CqaYd2eFOs2DbOHPWsFVQx+WmZD2phYS3dUusRob8IH+UpDFQk0pl0JDh30cwdB04t89/1O/w1cDnyilFU="]
+YOUR_CHANNEL_SECRET = os.environ["7d10760d5064ebdd0bceb9b15782aef2"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
